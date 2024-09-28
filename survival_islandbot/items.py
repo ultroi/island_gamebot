c506780813326643a9d230793d000b3cd4eb502a
Akshay Singh <86416271+ultroi@users.noreply.github.com>
2024-09-28 21:55:22 +0530

    Create items.py

---
+++ survival_islandbot/items.py
@@ -0,0 +1,16 @@
+FOOD_TYPES = {
+    "berries": 5,
+    "meat": 10,
+    "fish": 7
+}
+
+WEAPON_STATS = {
+    "spear": {"damage": 10},
+    "axe": {"damage": 15},
+    "bow": {"damage": 12}
+}
+
+ARMOR_STATS = {
+    "leather armor": {"protection": 5},
+    "chainmail armor": {"protection": 10}
+}
