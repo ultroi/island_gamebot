5aece691db2efb3df4439579fc89354254134e55
Akshay Singh <86416271+ultroi@users.noreply.github.com>
2024-11-25 22:22:39 +0530

    Delete BOT directory

--- BOT/config.py
+++
@@ -1,1 +0,0 @@
-MONGO_URI = "mongodb+srv://Akshayofficial:islandbot1904@tgbotproject.uwaxq.mongodb.net/Tgbotproject?retryWrites=true&w=majority"
--- BOT/handlers/help_handler.py
+++
@@ -1,7 +0,0 @@
-# handlers/help_handler.py
-from pyrogram import Client, filters
-from utils.text_responses import HELP_MESSAGE
-
-@Client.on_message(filters.command("help"))
-async def help_command(client, message):
-    await message.reply_text(HELP_MESSAGE)
